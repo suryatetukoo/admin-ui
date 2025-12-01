597ba843545eb593d5564b7b0bc597c741b71e1b
suryatetukoo <suryatetuko145@gmail.com>
2025-12-01 22:37:12 +0700

    UTS

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -5,13 +5,13 @@ import { staticPosts } from "./postsData";
 function Exercise() {
   return (
     <>
-      <div className="min-h-screen bg-gray-100 p-6">
-        <h1 className="text-3xl font-bold text-center mb-6 text-special-red">
+      <div className="min-h-screen bg-gray-100 p-6 px-48">
+        <h1 className="text-3xl font-bold text-center mb-6 text-special-red2">
           Post Cards
         </h1>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-6">
-          
+        <div className="grid md:grid-cols-2 lg:grid-cols-5 gap-6 max-w-full mx-auto">
+
           {staticPosts.map((post) => (
             <PostCard
               key={post.id}
